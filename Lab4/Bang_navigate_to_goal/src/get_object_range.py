cd8613617ccfca0793f95c74af47e156a3e4e153
Jaeho Bang <jaehob@andrew.cmu.edu>
2018-10-30 23:17:33 -0400

    Adding final code to repository

--- Lab4/Bang_navigate_to_goal/src/get_object_range.py
+++ Lab4/Bang_navigate_to_goal/src/get_object_range.py
@@ -1,71 +1,42 @@
 #!/usr/bin/env python
 # license removed for brevity
 import rospy
-import cv2
 import sys
 import numpy as np
 import math
-from std_msgs.msg import String
-from sensor_msgs.msg import CompressedImage
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import LaserScan
 
 
+
+# This class is going to take in an estimated
 class getObjectRange:
     def __init__(self, debug_mode):
         rospy.init_node('getObjectRange', anonymous=True, log_level=rospy.DEBUG)
         rospy.Subscriber("/scan", LaserScan, self.callback_laser, queue_size = 1)
 
         # TODO: Figure out the angle with some math formulation
-
-    #pixel_offset refers to distance of object from the center of image in terms of pixels
-    #pixel radius refers to radius of object in the image in terms of pixels
-    #return values will be in radians
-    def calculate_angle_range(self, pixel_offset, pixel_radius):
-        x2 = pixel_offset
-        x1 = pixel_radius
-        y1 = self.actual_ball_radius
-        y2 = x2 * y1 / x1
-        self.calculated_ball_offset = y2
-        if pixel_offset > 0:
-            self.calculated_angle_range = [2*math.pi - math.atan2(y2, self.assumed_ball_range[0]),                                     2*math.pi - math.atan2(y2, self.assumed_ball_range[1])]
-        else:
-            self.calculated_angle_range = [math.atan2(math.fabs(y2), self.assumed_ball_range[1]),
-                                           math.atan2(math.fabs(y2), self.assumed_ball_range[0])]
-        return
-
-    #will return a list of ranges because it might be sliced
-    def convertRad2Index(self, length_of_array, min_rad, max_rad, rad_increment):
-        #Note the LaserScan topic starts from 0 rad to pi rad
-        #0 rad refers to where the robot is facing, and value of rad increases as it goes cc-wise
-        #TODO: look at the orientation of the sensors and apply the appropriate transformation
-        start_index = int(self.calculated_angle_range[0] / rad_increment)
-        end_index = int(self.calculated_angle_range[1] / rad_increment)
-        return [start_index, end_index]
+        self.loc_publisher = rospy.Publisher("/turtlebot3/emergency_obstacle", Point, queue_size = 1)
+        self.emergency_dist = 0.5
+        self.publish_rate = rospy.Rate(10)
+        rospy.spin()
 
     def callback_laser(self, data):
-        print len(data.ranges)
-        print type(data.ranges)
-        '''
-        index_ranges = self.convertRad2Index(len(data.ranges),
-                                            data.angle_min, data.angle_max, data.angle_increment)
+        length = len(data.ranges)
+        left_limit = length / 12 #we will look at 0 - 30 degrees and 330 - 360 degrees
+        points_of_interest = np.array(data.ranges[:left_limit][::-1] + data.ranges[-left_limit:][::-1])
 
-        #rospy.loginfo("index ranges are " + str(index_ranges))
+        points_of_interest[points_of_interest < data.range_min] = float('inf')
+        ball_dist = min(points_of_interest)
 
-        minimum_dist = np.array(data.ranges[index_ranges[0]:index_ranges[1]])
-        minimum_dist[minimum_dist < data.range_min] = math.inf
+        if ball_dist < self.emergency_dist:
 
-        ball_dist = min(minimum_dist)
-
-        pub_point = Point()
-        pub_point.x = ball_dist
-        pub_point.y = self.calculated_ball_offset
-        pub_point.z = math.atan2(pub_point.y, pub_point.x) #calculated in radians
-
-        rospy.loginfo("x,y,z: " + str(pub_point.x) + "," + str(pub_point.y) + "," + str(pub_point.z))
-        self.loc_publisher.publish(pub_point)
-        self.publish_rate.sleep()
-        '''
+            pub_point = Point()
+            pub_point.x = ball_dist
+            pub_point.y = np.where(points_of_interest == ball_dist)[0][0] * 1.04 / len(points_of_interest) #60 degrees is 1.04 rad
+            rospy.loginfo("obs_location x: " + str(pub_point.x) + "m\t"  + "y: " + str(math.degrees(pub_point.y)) + " degrees\t")
+            self.loc_publisher.publish(pub_point)
+            self.publish_rate.sleep()
 
 
 
