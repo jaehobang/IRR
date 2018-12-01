c60de69c63d470addc3e5f9f54fbbe04bb239e17
Jaeho Bang <jaeho.bang@gmail.com>
2018-12-01 16:55:16 -0500

    Done with image node; need to change move node

--- Lab6/bang_lab6/src/bang_image_node.py
+++ Lab6/bang_lab6/src/bang_image_node.py
@@ -16,7 +16,8 @@ class ImageNode:
         """Train and wait for images to be passed
            Make the cropping and classification one pass
         """
-        rospy.init_node('image_node')
+        rospy.init_node('image_node', anonymous=True, log_level=rospy.DEBUG)
+        rospy.loginfo("inside init...")
         self.train_data = None
         self.train_labels = None
         self.img = None
@@ -24,13 +25,17 @@ class ImageNode:
         self.y = 50
         self.knn = None
         self.img = None
-        if not os.path.isdir("./imgs_mod"):
-          os.mkdir("imgs_mod")
+        self.project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        print(self.project_dir)
+        if not os.path.isdir( os.path.join(self.project_dir, "imgs_mod") ):
+          os.mkdir( os.path.join(self.project_dir, "imgs_mod") )
 
         self.train()
 
         # subscribes to images from camera
-        self.image_subscriber = rospy.Subscriber('/raspicam_node/image/compressed', CompressedImage, self.image_callback)
+        #TODO: For simulation we have to have /raspicam_node/image/compressed/compressed
+        #TODO: But for real one it should be /raspicam_node/image/compressed
+        self.image_subscriber = rospy.Subscriber('/raspicam_node/image/compressed/compressed', CompressedImage, self.image_callback)
         # subscribes to requests from the main loop
         self.request_subscriber = rospy.Subscriber('/turtlebot3/request_classification', String, self.request_callback)
 
@@ -40,6 +45,7 @@ class ImageNode:
         self.image_setup_publisher = rospy.Publisher("/turtlebot3/image_node_setup", String, queue_size = 1)
 
         self.image_setup_publisher.publish( String("foo") )
+        rospy.loginfo("Done init... now spinning...")
         rospy.spin()
 
 
@@ -74,7 +80,7 @@ class ImageNode:
 
     def preprocess_on_fly(self, img):
 
-        height, width = img.shape
+        height, width = img.shape[:2]
 
         (thresh, img_bin) = cv2.threshold(img, 100, 255,
                                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # Thresholding the image
@@ -112,13 +118,12 @@ class ImageNode:
         (contours, boundingBoxes) = self.sort_contours(contours, method="top-to-bottom")
 
         idx = 0
-        print("Contour count is " + str(len(contours)))
         new_imgs = []
         for c in contours:
             # Returns the location and width,height for every contour
             x, y, w, h = cv2.boundingRect(c)
             # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
-            if (w > width / 5 and h > height / 5 and w < width and h < height):
+            if (w > width / 20 and h > height / 20 ):
                 idx += 1
                 new_img = img[y:y + h, x:x + w]
                 new_imgs.append(new_img)
@@ -126,80 +131,97 @@ class ImageNode:
         if new_imgs == []:
             new_imgs.append(img)
 
-        return new_imgs[0]
+        return new_imgs
 
-    def request_callback(self):
-        curr_image = self.img
-        crop = self.preprocess_on_fly(curr_image)
+    def request_callback(self, data):
+        rospy.loginfo("request received!!")
+        np_arr = np.fromstring(self.img, np.uint8)
+        curr_image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
 
-        test_img = crop.flatten().reshape(1, 33 * 25)
-        test_img = test_img.astype(np.float32)
+        crops = self.preprocess_on_fly(curr_image)
+        resultss = []
+        for i in xrange(len(crops)):
+            crop = crops[i]
+            cv2.imwrite( os.path.join(self.project_dir, 'temp' + str(i) + '.png'), crops[i])
+            rospy.loginfo("Wrote cropped image to " + os.path.join(self.project_dir, 'temp' + str(i) + '.png'))
+            crop = np.array(cv2.resize(crop, (33, 25)))
 
-        ret, results, neighbours, dist = self.knn.findNearest(test_img, 3)
+            test_img = crop.flatten().reshape(1, 33 * 25)
+            test_img = test_img.astype(np.float32)
 
-        self.classification_publisher.publish(ret)
+            ret, results, neighbours, dist = self.knn.findNearest(test_img, 3)
+            resultss.append(str(ret))
+
+        #access the ones that are not zero
+        rospy.loginfo("results are " + str(resultss))
+        for result in resultss:
+            if eval(result) != 0:
+                rospy.loginfo("return label is...." + str(result))
+                self.classification_publisher.publish(str(result))
+                return
+        rospy.loginfo("return label is...." + str(0))
+        self.classification_publisher.publish(str(0))
 
 
 
     def train(self):
+        for file in ['imgs/train.txt', 'imgs/test.txt']:
+            with open( os.path.join(self.project_dir, file) , 'rb') as f:
+                reader = csv.reader(f)
+                lines = list(reader)
+            for i in range(len(lines)):  # len(lines)
+                img = cv2.imread( os.path.join(self.project_dir, "imgs", lines[i][0] + ".png") , 0)
+                height, width = img.shape
 
-        with open('./img/train.txt', 'rb') as f:
-            reader = csv.reader(f)
-            lines = list(reader)
-        for i in range(len(lines)):  # len(lines)
-            img = cv2.imread("./imgs/" + lines[i][0] + ".png", 0)
-            height, width = img.shape
-
-            (thresh, img_bin) = cv2.threshold(img, 100, 255,
-                                              cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # Thresholding the image
-            img_bin = 255 - img_bin  # Invert the image
-
-
-            # Defining a kernel length
-            kernel_length = np.array(img).shape[1] // 40
-
-            # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
-            verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
-            # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
-            hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
-            # A kernel of (3 X 3) ones.
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            # Morphological operation to detect verticle lines from an image
-            img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
-            verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
-
-             # Morphological operation to detect horizontal lines from an image
-            img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
-            horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
-            # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
-            alpha = 0.5
-            beta = 1.0 - alpha
-            # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
-            img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
-            img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
-            (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-            # For Debugging
-            # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
-            # Find contours for image, which will detect all the boxes
-            im2, contours, hierarchy = cv2.findContours(
-                img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            # Sort all the contours by top to bottom.
-            (contours, boundingBoxes) = self.sort_contours(contours, method="top-to-bottom")
+                (thresh, img_bin) = cv2.threshold(img, 100, 255,
+                                                  cv2.THRESH_BINARY | cv2.THRESH_OTSU)  # Thresholding the image
+                img_bin = 255 - img_bin  # Invert the image
 
 
-            idx = 0
-            print("Contour count is " + str(len(contours)))
-            for c in contours:
-                # Returns the location and width,height for every contour
-                x, y, w, h = cv2.boundingRect(c)
-                # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
-                if (w > width / 5 and h > height / 5 and w < width and h < height):
-                    idx += 1
-                    new_img = img[y:y + h, x:x + w]
-                    cv2.imwrite('./imgs_mod/' + lines[i][0] + '_' + str(idx) + '.png', new_img)
+                # Defining a kernel length
+                kernel_length = np.array(img).shape[1] // 40
+
+                # A verticle kernel of (1 X kernel_length), which will detect all the verticle lines from the image.
+                verticle_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, kernel_length))
+                # A horizontal kernel of (kernel_length X 1), which will help to detect all the horizontal line from the image.
+                hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_length, 1))
+                # A kernel of (3 X 3) ones.
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+                # Morphological operation to detect verticle lines from an image
+                img_temp1 = cv2.erode(img_bin, verticle_kernel, iterations=3)
+                verticle_lines_img = cv2.dilate(img_temp1, verticle_kernel, iterations=3)
+
+                 # Morphological operation to detect horizontal lines from an image
+                img_temp2 = cv2.erode(img_bin, hori_kernel, iterations=3)
+                horizontal_lines_img = cv2.dilate(img_temp2, hori_kernel, iterations=3)
+                # Weighting parameters, this will decide the quantity of an image to be added to make a new image.
+                alpha = 0.5
+                beta = 1.0 - alpha
+                # This function helps to add two image with specific weight parameter to get a third image as summation of two image.
+                img_final_bin = cv2.addWeighted(verticle_lines_img, alpha, horizontal_lines_img, beta, 0.0)
+                img_final_bin = cv2.erode(~img_final_bin, kernel, iterations=2)
+                (thresh, img_final_bin) = cv2.threshold(img_final_bin, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                # For Debugging
+                # Enable this line to see verticle and horizontal lines in the image which is used to find boxes
+                # Find contours for image, which will detect all the boxes
+                im2, contours, hierarchy = cv2.findContours(
+                    img_final_bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                # Sort all the contours by top to bottom.
+                (contours, boundingBoxes) = self.sort_contours(contours, method="top-to-bottom")
 
 
-        with open('./imgs/train.txt', 'rb') as f:
+                idx = 0
+                for c in contours:
+                    # Returns the location and width,height for every contour
+                    x, y, w, h = cv2.boundingRect(c)
+                    # If the box height is greater then 20, widht is >80, then only save it as a box in "cropped/" folder.
+                    if (w > width / 5 and h > height / 5 and w < width and h < height):
+                        idx += 1
+                        new_img = img[y:y + h, x:x + w]
+                        cv2.imwrite( os.path.join(self.project_dir, 'imgs_mod', lines[i][0] + '_' + str(idx) + '.png'), new_img)
+
+
+        with open( os.path.join(self.project_dir, 'imgs', 'train.txt'), 'rb') as f:
             reader = csv.reader(f)
             lines = list(reader)
 
@@ -207,12 +229,13 @@ class ImageNode:
         for i in range(len(lines)):
             try:
                 if lines[i][1] != '0':
-                    train.append(np.array(cv2.resize(cv2.imread("./imgs_mod/" + lines[i][0] + "_1.png", 0), (33, 25))))
+
+                    train.append(np.array(cv2.resize(cv2.imread( os.path.join(self.project_dir, "imgs_mod",  lines[i][0] + "_1.png"), 0), (33, 25))))
                 else:
-                    train.append(np.array(cv2.resize(cv2.imread("./imgs/" + lines[i][0] + ".png", 0), (33, 25))))
+                    train.append(np.array(cv2.resize(cv2.imread( os.path.join(self.project_dir, "imgs", lines[i][0] + ".png"), 0), (33, 25))))
 
             except:
-                a = cv2.imread("./imgs_mod/" + lines[i][0] + "_1.png", 0)
+                a = cv2.imread(os.path.join(self.project_dir, "imgs", lines[i][0] + ".png"), 0)
                 print(lines[i])
                 print(a.shape)
 
@@ -230,6 +253,46 @@ class ImageNode:
         self.knn.train(train_data, cv2.ml.ROW_SAMPLE, train_labels)
 
 
+        with open( os.path.join(self.project_dir, 'imgs', 'test.txt'), 'rb') as f:
+            reader = csv.reader(f)
+            lines = list(reader)
+
+        correct = 0.0
+        confusion_matrix = np.zeros((6, 6))
+
+        for i in range(len(lines)):
+            directory = os.path.join(self.project_dir, "imgs_mod" , lines[i][0] + "_1.png")
+            rospy.loginfo("-----------------" + directory)
+            if lines[i][1] != '0':
+
+                test_img = np.array(cv2.resize(cv2.imread( os.path.join(self.project_dir, "imgs_mod" , lines[i][0] + "_1.png"), 0), (33, 25)))
+            else:
+                test_img = np.array(cv2.resize(cv2.imread( os.path.join(self.project_dir, "imgs" , lines[i][0] + ".png"), 0), (33, 25)))
+
+            test_img = test_img.flatten().reshape(1, 33 * 25)
+            test_img = test_img.astype(np.float32)
+
+            test_label = np.int32(lines[i][1])
+
+            ret, results, neighbours, dist = self.knn.findNearest(test_img, 3)
+
+            if test_label == ret:
+                print(lines[i][0], " Correct, ", ret)
+                correct += 1
+                confusion_matrix[np.int32(ret)][np.int32(ret)] += 1
+            else:
+                confusion_matrix[test_label][np.int32(ret)] += 1
+
+                print(lines[i][0], " Wrong, ", test_label, " classified as ", ret)
+                print "\tneighbours: ", neighbours
+                print "\tdistances: ", dist
+
+        print("\n\nTotal accuracy: ", correct / len(lines))
+        print(confusion_matrix)
+
+
         return
 
 
+if __name__ == "__main__":
+    imagenode = ImageNode()
--- Lab6/bang_lab6/src/main.py
+++ Lab6/bang_lab6/src/main.py
@@ -58,7 +58,11 @@ class goToGoal:
     self.turn_rate = rospy.Rate(1)
 
     self.check_direction = None
+    self.dist_to_image = 0.4 #distance we need to be to classify image
+    self.move_back_dist = 0.5 - self.dist_to_image  # distance we need to move back after classifying image
 
+    self.performing_higher_move = False
+    self.request_once = False
 
 
     self.init_scheme()
@@ -74,56 +78,72 @@ class goToGoal:
 
   def callback_laser(self, data):
     #This is when we have arrived at a stop after turning and need to check for delta turning
-    if self.check_direction is not None:
-      if self.check_direction == "left":
-        length = len(data.ranges)
-        right_limit = 8 * length / 36
-        left_limit = 10 * length / 36
+    if self.performing_higher_move == False:
 
-      if self.check_direction == "right":
-        length = len(data.ranges)
-        right_limit = 26 * length / 36
-        left_limit = 28 * length / 36
+      if self.check_direction is not None:
+        if self.check_direction == "front":
+          #TODO: do the logic
+          length = len(data.ranges)
+          left_limit = length / 36
+          points_of_interest = np.array(data.ranges[:left_limit][::-1] + data.ranges[-left_limit:][::-1])
+          points_of_interest[points_of_interest < data.range_min] = 0
+          wall_dist = sum(points_of_interest) / len(points_of_interest[points_of_interest != 0])
 
-      left_range = np.array(data.ranges[left_limit:left_limit + 5])
-      right_range = np.array(data.ranges[right_limit - 5:right_limit])
-      left_range[left_range < data.range_min] = 0
-      right_range[right_range < data.range_min] = 0
-      left_avg = sum(left_range) / len(left_range[left_range != 0])
-      right_avg = sum(right_range) / len(right_range[right_range != 0])
+          if wall_dist < self.dist_to_image + self.move_back_dist:
+            self.move_back_a_little()
 
-      eval_return = self.evaluate_with_epsilon(left_avg, right_avg)
-      if eval_return == "left":
-        self.turn_a_bit("right") #TODO
-      elif eval_return == "right":
-        self.turn_a_bit("left") #TODO
-      else: #eval_return == "equal"
-        assert(eval_return == "equal")
-        self.stop_wheels()
-        self.check_direction = None
-    #this is when we are going forward
-    else:
-      length = len(data.ranges)
-      left_limit = length / 36  # we will look at 0 - 10 degrees and 350 - 360 degrees
-      points_of_interest = np.array(data.ranges[:left_limit][::-1] + data.ranges[-left_limit:][::-1])
+        else:
+          if self.check_direction == "left":
+            length = len(data.ranges)
+            right_limit = 8 * length / 36
+            left_limit = 10 * length / 36
 
-      points_of_interest[points_of_interest < data.range_min] = 0
-      wall_dist = sum(points_of_interest) / len(points_of_interest[points_of_interest != 0])
+          if self.check_direction == "right":
+            length = len(data.ranges)
+            right_limit = 26 * length / 36
+            left_limit = 28 * length / 36
 
-      if wall_dist > 0.3: # one foot away from the sign
-        self.keep_going() #TODO
+          left_range = np.array(data.ranges[left_limit:left_limit + 5])
+          right_range = np.array(data.ranges[right_limit - 5:right_limit])
+          left_range[left_range < data.range_min] = 0
+          right_range[right_range < data.range_min] = 0
+          left_avg = sum(left_range) / len(left_range[left_range != 0])
+          right_avg = sum(right_range) / len(right_range[right_range != 0])
+
+          eval_return = self.evaluate_with_epsilon(left_avg, right_avg)
+          if eval_return == "left":
+            self.turn_a_bit("right") #TODO
+          elif eval_return == "right":
+            self.turn_a_bit("left") #TODO
+          else: #eval_return == "equal"
+            assert(eval_return == "equal")
+            self.stop_wheels()
+            self.check_direction = None
+      #this is when we are going forward
       else:
-        self.stop_wheels()
-        self.request_classification() #TODO
+        length = len(data.ranges)
+        left_limit = length / 36  # we will look at 0 - 10 degrees and 350 - 360 degrees
+        points_of_interest = np.array(data.ranges[:left_limit][::-1] + data.ranges[-left_limit:][::-1])
+
+        points_of_interest[points_of_interest < data.range_min] = 0
+        wall_dist = sum(points_of_interest) / len(points_of_interest[points_of_interest != 0])
+
+        if wall_dist > self.dist_to_image: # one foot away from the sign
+          self.keep_going()
+        else:
+          self.stop_wheels()
+          self.request_classification()
 
   def turn_a_bit(self, direction):
+    self.performing_higher_move = True
     twist = Twist()
     if direction == "right":
-      twist.z = -0.5
+      twist.linear.z = -0.5
     elif direction == "left":
-      twist.z = 0.5
+      twist.linear.z = 0.5
     self.vel_publisher.publish(twist)
     self.publish_rate.sleep()
+    self.performing_higher_move = False
 
   def evaluate_with_epsilon(self, left_v, right_v):
     epsilon = 0.05
@@ -134,26 +154,47 @@ class goToGoal:
     else:
       return "right"
 
-  def keep_going(self):
+  def move_back_a_little(self):
+    self.performing_higher_move = True
     twist = Twist()
-    twist.x = 0.5
+    twist.linear.x = -self.move_back_dist
+    self.vel_publisher.publish(twist)
+    self.publish_rate.sleep()
+    self.performing_higher_move = False
+
+  def move_back(self):
+    self.performing_higher_move = True
+    twist = Twist()
+    twist.linear.x = -self.move_back_dist
+    self.vel_publisher.publish(twist)
+    self.turn_rate.sleep()
+    self.performing_higher_move = False
+
+  def keep_going(self):
+    self.performing_higher_move = True
+    twist = Twist()
+    twist.linear.x = 0.1
 
     self.vel_publisher.publish(twist)
     self.publish_rate.sleep()
+    self.performing_higher_move = False
 
   def stop_wheels(self):
+    self.performing_higher_move = True
     twist = Twist()
-    twist.x = 0
+    twist.linear.x = 0
     self.vel_publisher.publish(twist)
     self.publish_rate.sleep()
+    self.performing_higher_move = False
 
   def turn_90_degrees(self, direction):
+    self.performing_higher_move = True
     twist = Twist()
     if direction == 'left':
 
-      twist.z = 1.57
+      twist.linear.z = 1.57
     if direction == 'right':
-      twist.z = -1.57
+      twist.linear.z = -1.57
 
     self.vel_publisher.publish(twist)
     self.turn_rate.sleep()
@@ -163,21 +204,27 @@ class goToGoal:
       self.check_direction = 'right'
     if direction == 'right':
       self.check_direction = 'left'
+    self.performing_higher_move = False
 
   def turn_180_degrees(self):
+    self.performing_higher_move = True
     twist = Twist()
-    twist.z = 1.57
+    twist.linear.z = 1.57
     self.vel_publisher.publish(twist)
     for i in xrange(2): self.turn_rate.sleep()
     #Is there a way to check turn rate delta??? skip for now
+    self.performing_higher_move = False
 
   def request_classification(self):
+    if self.request_once == False:
+      rospy.loginfo("Requesting image classification")
 
-    str = String("foo")
-    self.classification_publisher.publish(str)
-    self.publish_rate.sleep()
+      str = String("foo")
+      self.classification_publisher.publish(str)
+      self.publish_rate.sleep()
+      self.request_once = True
 
-  def get_classification_and_drive(self, data):
+  def classification_callback(self, data):
     # data should be a number according to what is available in the train.txt, test.txt
     #   -> we will convert it in this function
     # '0' -> no sign
@@ -187,21 +234,27 @@ class goToGoal:
     # '4' -> stop
     # '5' -> goal (exit_program)
   # "move_left", "move_right", "stop", "done"
-    if data.data == '0':
+    rospy.loginfo("classification result is" + data.data)
+    if eval(data.data) == 0:
+      self.move_back_a_little()
       self.request_classification()
-    elif data.data == '1':
+    elif eval(data.data) == 1:
+      self.move_back()
       self.turn_90_degrees('left')
-    elif data.data == '2':
+    elif eval(data.data) == 2:
+      self.move_back()
       self.turn_90_degrees('right')
-    elif data.data == '3' or data.data == '4':
+    elif eval(data.data) == 3 or eval(data.data) == 4:
+      self.move_back()
       self.turn_180_degrees()
-    elif data.data == '5':
+    elif eval(data.data) == 5:
       self.stop_wheels()
       rospy.signal_shutdown("Reached Goal!")
+    self.request_once = False
 
   def init_scheme(self): #Here we will wait for image node to finish its classification process
     rospy.loginfo("Waiting for message from image classifier...")
-    rospy.wait_for_message("/turtlebot3/image_node_setup", String)
+    #rospy.wait_for_message("/turtlebot3/image_node_setup", String)
     rospy.loginfo("Message received, training done, we are ready to go")
 
 
